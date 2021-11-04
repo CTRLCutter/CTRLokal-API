86f46b1e61182065aa055d849084e0fbd8c4c01b
Lorenz Seufert <lorenzseufert@gmail.com>
2021-11-04 12:52:19 +0100

    :sparkles: Inital commit

---
+++ src/main/java/de/ctrlcutter/api/ctrlokalapi/CtrLokalApiApplication.java
@@ -0,0 +1,13 @@
+package de.ctrlcutter.api.ctrlokalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrLokalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtrLokalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ctrlcutter/api/ctrlokalapi/CtrLokalApiApplicationTests.java
@@ -0,0 +1,12 @@
+package de.ctrlcutter.api.ctrlokalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrLokalApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
