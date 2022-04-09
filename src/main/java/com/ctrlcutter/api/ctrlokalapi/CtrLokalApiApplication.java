3f4247ba405484ee55c7ad1ad826c652e93d3255
Lorenz Seufert <lorenzseufert@gmail.com>
2022-04-09 20:45:58 +0200

    added annotation for persistence

--- src/main/java/com/ctrlcutter/api/ctrlokalapi/CtrLokalApiApplication.java
+++ src/main/java/com/ctrlcutter/api/ctrlokalapi/CtrLokalApiApplication.java
@@ -2,9 +2,11 @@ package com.ctrlcutter.api.ctrlokalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan("com.ctrlcutter.*")
 @ComponentScan(basePackages = {"com.ctrlcutter.backend.*", "com.ctrlcutter.api.ctrlokalapi.*"})
 public class CtrLokalApiApplication {
 
