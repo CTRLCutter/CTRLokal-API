0507f39f8350d8a1b8da462b74ef7371dc2e3e81
Lorenz Seufert <lorenzseufert@gmail.com>
2022-04-10 19:22:43 +0200

    added get request for basic and predefined scripts in one

--- src/main/java/com/ctrlcutter/api/ctrlokalapi/controller/PersistenceController.java
+++ src/main/java/com/ctrlcutter/api/ctrlokalapi/controller/PersistenceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ctrlcutter.backend.dto.BasicPreDefinedDTO;
 import com.ctrlcutter.backend.persistence.model.BasicHotstringScript;
 import com.ctrlcutter.backend.persistence.model.BasicScript;
 import com.ctrlcutter.backend.persistence.model.PreDefinedScript;
@@ -57,5 +58,18 @@ public class PersistenceController {
         }
 
         return new ResponseEntity<>(preDefinedScripts, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/allBasicPreDefined", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BasicPreDefinedDTO> getAllBasicPreDefinedScripts() {
+        BasicPreDefinedDTO basicPreDefinedDTO = new BasicPreDefinedDTO();
+        basicPreDefinedDTO.setBasicScripts(this.persistenceReceiveService.getAllBasicScripts());
+        basicPreDefinedDTO.setPreDefinedScripts(this.persistenceReceiveService.getAllPreDefinedScripts());
+
+        if (basicPreDefinedDTO.getBasicScripts().isEmpty() && basicPreDefinedDTO.getPreDefinedScripts().isEmpty()) {
+            return new ResponseEntity<>(basicPreDefinedDTO, HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(basicPreDefinedDTO, HttpStatus.OK);
     }
 }
