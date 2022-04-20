1fe491d2d5de31a8ab5fa75d95e744e963a36e3a
Lorenz Seufert <lorenzseufert@gmail.com>
2022-04-20 16:44:03 +0200

    added web backup and environment variables for web backup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CTRLokal-API
+
+## Environment variables
+
+Following environment variables are necessary for the web backup:
+
+- WEB_URL - full http url to save the scripts
+- WEB_USERNAME - username of the web api user
+- WEB-PASSWORD - password of the web api user
--- src/main/java/com/ctrlcutter/api/ctrlokalapi/controller/PersistenceController.java
+++ src/main/java/com/ctrlcutter/api/ctrlokalapi/controller/PersistenceController.java
@@ -1,30 +1,33 @@
 package com.ctrlcutter.api.ctrlokalapi.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.ctrlcutter.backend.dto.AnonymizedScriptDTO;
 import com.ctrlcutter.backend.dto.BasicPreDefinedDTO;
 import com.ctrlcutter.backend.persistence.model.BasicHotstringScript;
 import com.ctrlcutter.backend.persistence.model.BasicScript;
 import com.ctrlcutter.backend.persistence.model.PreDefinedScript;
 import com.ctrlcutter.backend.persistence.service.PersistenceReceiveService;
+import com.ctrlcutter.backend.persistence.service.PersistenceSaveService;
 
 @RestController
 @RequestMapping("/storage")
 public class PersistenceController {
 
-    private PersistenceReceiveService persistenceReceiveService;
+    private final PersistenceReceiveService persistenceReceiveService;
+    private final PersistenceSaveService persistenceSaveService;
 
     @Autowired
-    public PersistenceController(PersistenceReceiveService persistenceReceiveService) {
+    public PersistenceController(PersistenceReceiveService persistenceReceiveService, PersistenceSaveService persistenceSaveService) {
         this.persistenceReceiveService = persistenceReceiveService;
+        this.persistenceSaveService = persistenceSaveService;
     }
 
     @GetMapping(value = "/allBasic", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -71,5 +74,24 @@ public class PersistenceController {
         }
 
         return new ResponseEntity<>(basicPreDefinedDTO, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/backupToWeb", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> backupScripts(@RequestHeader Map<String, String> header) {
+        String sessionKey = header.get("sessionkey");
+
+        if (sessionKey == null || sessionKey.isEmpty()) {
+            return new ResponseEntity<>("Missing session key", HttpStatus.UNAUTHORIZED);
+        }
+
+        List<AnonymizedScriptDTO> anonymizedScripts = this.persistenceSaveService.anonymizeScriptsForBackup();
+
+        if (!anonymizedScripts.isEmpty()) {
+            ResponseEntity<String> response = this.persistenceSaveService.saveScriptsToWeb(sessionKey, anonymizedScripts);
+
+            return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+        } else {
+            return new ResponseEntity<>("No scripts to backup", HttpStatus.NO_CONTENT);
+        }
     }
 }
